f348ccd1704da83102166da3ba58b8549b689350
T.J. Crowder <tj@crowdersoftware.com>
2024-11-04 13:43:18 +0000

    Add project bootstrapped by Vite with my-element.ts updated to replicate the issue.

---
+++ src/my-element.ts
@@ -0,0 +1,28 @@
+import { html, LitElement } from "lit";
+import { customElement, state } from "lit/decorators.js";
+import { asyncAppend } from "lit/directives/async-append.js";
+
+async function* tossCoins(count: number) {
+    for (let i = 0; i < count; i++) {
+        yield Math.random() > 0.5 ? "Heads" : "Tails";
+        await new Promise((r) => setTimeout(r, 1000));
+    }
+}
+
+@customElement("my-element")
+class MyElement extends LitElement {
+    @state()
+    private tosses = tossCoins(10);
+
+    render() {
+        return html` <ul>
+            ${asyncAppend(this.tosses, (v: string) => html`<li>${v}</li>`)}
+        </ul>`;
+    }
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        "my-element": MyElement;
+    }
+}
